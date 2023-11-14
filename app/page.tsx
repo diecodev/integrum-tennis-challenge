9a970d3d682df2ce12c8f42ed97ee3e7a2c2ca5d
diecodev <diegodiaz05@gmail.com>
2023-11-13 22:45:21 -0500

    Setup homepage user redirect

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,14 @@
+import { auth } from "@clerk/nextjs";
+import { ADMIN_EMAIL } from "@root/const";
+import { redirect } from "next/navigation";
+
 export default function Home() {
+  const { sessionClaims } = auth();
+
+  if (sessionClaims?.primaryEmail !== ADMIN_EMAIL) {
+    redirect("/find");
+  }
+
   return (
     <main>
       <h1>Tennis League</h1>
