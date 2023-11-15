135a4f1d6b139ce36b2308ee82a3a0d699b3e119
diecodev <diegodiaz05@gmail.com>
2023-11-15 01:00:02 -0500

    Add status colors

--- components/general/status.tsx
+++ components/general/status.tsx
@@ -5,7 +5,9 @@ export function StatusLabel({ status }: { status: Statuses }) {
   return (
     <span
       className={clsx("rounded-full px-3 font-medium", {
-        "bg-yellow-300": status === "OPEN",
+        "bg-green-300": status === "OPEN",
+        "bg-gray-300": status === "FINISHED",
+        "bg-yellow-300": status === "PLAYING",
       })}
     >
       {status}
